45404eef2f7971f10495e14fdfcf7af0bb8b5c55
renzhifan <zhifan6797@163.com>
2019-04-18 11:02:58 +0800

    1.初始化数据库
    数据填充
    
    进入 tinker 环境：
    
    $ php artisan tinker
    
    执行以下语句，填充假数据：
    
    >>> factory('App\Reply',50)->create();

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -18,7 +18,31 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => $password ?: $password = bcrypt('123456'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Thread::class, function (Faker\Generator $faker) {
+
+    return [
+        'user_id' => function(){
+            return factory('App\User')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+    ];
+});
+
+$factory->define(App\Reply::class, function (Faker\Generator $faker) {
+
+    return [
+        'thread_id' => function(){
+            return factory('App\Thread')->create()->id;
+        },
+        'user_id' => function(){
+            return factory('App\User')->create()->id;
+        },
+        'body' => $faker->paragraph,
     ];
 });
